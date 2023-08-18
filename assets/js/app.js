88474f8d08eea765a19b864c309b9f9ee62783a9
Mauricio Peniche <mauricio.peniche@gmail.com>
2023-08-18 10:51:20 -0600

    actualizaciones a la revision de codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,27 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+const $n = document.querySelector('.name');  // Se corrigió el nombre del selector
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) {  // Faltaba la palabra clave 'async'
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  try {  // Se agrega la estructura try para manejar errores
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json();  // Se agrega el await
+    console.log(data);
+    $n.textContent = data.name;  // Se quitaron las comillas simples
+    $b.textContent = data.blog;  // "           "                "
+    $l.textContent = data.location;  // "         "              "
+  } catch (err) {  // Se agregó 'catch' para manejar errores
+    handleError(err);
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`;  // Agregada la '$' antes de 'n'
 }
 
 displayUser('stolinski').catch(handleError);
